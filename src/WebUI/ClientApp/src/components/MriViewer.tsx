1ed2fad6bcb6ad07265c9f546e68da6fe97ecf8c
Benedikt Reuter <benedikt.reuter@zuehlke.com>
2023-04-30 23:25:43 +0200

    fix mri viewer as well

--- src/WebUI/ClientApp/src/components/MriViewer.tsx
+++ src/WebUI/ClientApp/src/components/MriViewer.tsx
@@ -2,7 +2,7 @@ import { useRef, useEffect } from 'react';
 import { Niivue } from '@niivue/niivue';
 
 export const MriViewer = () => {
-	const canvas: any = useRef();
+	const canvas = useRef<HTMLCanvasElement>(null);
 	useEffect(() => {
 		const volumeList = [
 			{
@@ -12,14 +12,13 @@ export const MriViewer = () => {
 		const nv = new Niivue({ isResizeCanvas: false });
 		nv.attachToCanvas(canvas.current);
 		nv.loadVolumes(volumeList).then(() =>
-			console.log(canvas.current.toDataURL())
+			console.log(canvas.current?.toDataURL())
 		);
 	}, []);
 
 	return (
-		<div>
-			{' '}
-			<canvas ref={canvas} height={640} width={640} />{' '}
+		<div className="m-6">
+			<canvas ref={canvas} height={640} width={640} />
 		</div>
 	);
 };
