9ca7cc2541ce0ea7aa55c902393afd05d2896cc3
Benedikt Reuter <benedikt.reuter@zuehlke.com>
2023-05-01 01:46:03 +0200

    add some further linting rules (untested)

--- src/WebUI/ClientApp/.eslintrc.ts
+++ src/WebUI/ClientApp/.eslintrc.ts
@@ -2,13 +2,22 @@ module.exports = {
 	env: { browser: true, es2020: true },
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
 		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jsx-a11y/recommended',
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-	plugins: ['react-refresh'],
+	plugins: ['react-refresh', 'jsx-a11y'],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
 	rules: {
 		'react-refresh/only-export-components': 'warn',
+		'jsx-a11y/rule-name': 2,
 	},
 };
