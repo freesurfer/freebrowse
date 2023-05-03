6f62ce7a05a55bd5b5105e50fbb40a99c6d7aa30
Benedikt Reuter <benedikt.reuter@zuehlke.com>
2023-05-03 12:02:27 +0200

    fix test command

--- src/WebUI/ClientApp/lint-staged.cjs
+++ src/WebUI/ClientApp/lint-staged.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	'*.{js,jsx,ts,tsx}': [
 		'eslint --max-warnings=0',
-		'test --coverage false',
+		'jest --coverage false',
 		() => 'tsc-files --noEmit',
 	],
 	'*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
