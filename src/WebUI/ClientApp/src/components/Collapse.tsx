310f71f672f8d800e08add07c89d7a72bfe3735a
Benedikt Reuter <benedikt.reuter@zuehlke.com>
2023-06-19 12:19:17 +0200

    prevent open animation on initial collapse state

--- src/WebUI/ClientApp/src/components/Collapse.tsx
+++ src/WebUI/ClientApp/src/components/Collapse.tsx
@@ -16,7 +16,7 @@ export const Collapse = ({
 	initialState?: boolean;
 }): React.ReactElement => {
 	const { getCollapseProps, getToggleProps, isExpanded, setExpanded } =
-		useCollapse();
+		useCollapse({ defaultExpanded: initialState });
 
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	useEffect(() => setExpanded(initialState), []);
