50bd7026b04dd88dc1b0fa7943222f9de2fb4ab1
Paul Wighton <paul.wighton@gmail.com>
2025-11-14 17:20:10 -0500

    [ENH] get examples script also gets aparc.a2009s+aseg.mgz

--- data/get-example-volumes.sh
+++ data/get-example-volumes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p volumes
-wget https://niivue.github.io/niivue-demo-images/mni152.nii.gz -P volumes/
-wget https://niivue.github.io/niivue-demo-images/pcasl.nii.gz -P volumes/
-wget https://niivue.com/demos/images/hippo.nii.gz -P volumes/
-
+wget -nc https://niivue.github.io/niivue-demo-images/mni152.nii.gz -P volumes/
+wget -nc https://niivue.github.io/niivue-demo-images/pcasl.nii.gz -P volumes/
+wget -nc https://niivue.com/demos/images/hippo.nii.gz -P volumes/
+wget -nc https://niivue.com/demos/images/aparc.a2009s+aseg.mgz -P volumes/
 
