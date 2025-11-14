fd5a710791de0a0be07d52e39e1030d27800152c
Paul Wighton <paul.wighton@gmail.com>
2025-11-14 13:40:25 -0500

    [GIT] rebase

--- data/get-example-data.sh
+++ data/get-example-data.sh
@@ -5,3 +5,10 @@ wget -nc https://niivue.github.io/niivue-demo-images/pcasl.nii.gz -P volumes/
 wget -nc https://niivue.com/demos/images/hippo.nii.gz -P volumes/
 wget -nc https://niivue.com/demos/images/aparc.a2009s+aseg.mgz -P volumes/
 
+mkdir -p surf-eg
+wget -nc https://niivue.com/demos/images/fs/brainmask.mgz -P surf-eg/
+wget -nc https://niivue.com/demos/images/fs/wm.mgz -P surf-eg/
+wget -nc https://niivue.com/demos/images/fs/rh.pial -P surf-eg/
+wget -nc https://niivue.com/demos/images/fs/lh.pial -P surf-eg/
+wget -nc https://niivue.com/demos/images/fs/lh.white -P surf-eg/
+wget -nc https://niivue.com/demos/images/fs/rh.white -P surf-eg/
