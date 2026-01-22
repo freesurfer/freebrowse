34db958b1943491f0bfc92b603dcbe81e4ff0b8d
Paul Wighton <paul.wighton@gmail.com>
2026-01-21 19:13:42 -0500

    [ENH] updating get-example-data.sh

--- data/get-example-data.sh
+++ data/get-example-data.sh
@@ -12,3 +12,7 @@ wget -nc https://niivue.com/demos/images/fs/rh.pial -P surf-eg/
 wget -nc https://niivue.com/demos/images/fs/lh.pial -P surf-eg/
 wget -nc https://niivue.com/demos/images/fs/lh.white -P surf-eg/
 wget -nc https://niivue.com/demos/images/fs/rh.white -P surf-eg/
+
+mkdir -p 4d-eg
+wget -nc https://pwighton.github.io/freebrowse-test-data/pet/tpl-MNI305_T1w.nii.gz -P 4d-eg/
+wget -nc https://pwighton.github.io/freebrowse-test-data/pet/sub-PS11_ses-baselinebrain_space-mni305_pvc-nopvc_desc-preproc_pet.nii.gz -P 4d-eg/
